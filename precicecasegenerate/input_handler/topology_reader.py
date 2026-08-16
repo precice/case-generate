27329e93a737f327d1e9810d33ba9782c910d84d
Orlando <orlac2700@gmail.com>
2026-08-16 08:53:08 +0200

    Update topology_reader.py
    Extend preprocessing to guard against multiple location-types for the same location
    and actually implement the check for "unique-exchanges", which the docstring said the method does, but never actually did.

--- precicecasegenerate/input_handler/topology_reader.py
+++ precicecasegenerate/input_handler/topology_reader.py
@@ -56,10 +56,12 @@ class TopologyReader:
         - participant names are unique.
         - exchanges only contain known "to" and "from" participants.
         - exchanges are unique, when ignoring "to-location-name", "from-location-name" and "type" tags.
+        - locations of the same participant are not defined with different "location-type" tags. If a location has its type defined only once, the type is written to the topology in all other exchanges where it was not defined.
 
         If any of these checks fail, an error message is printed and the program is aborted.
         Additionally, it is checked if any of the data names contains one of the uniquifiers defined in
-        helper.DATA_UNIQUIFIERS. If so, this uniquifier is removed from the list of uniquifiers.
+        helper.DATA_UNIQUIFIERS. If so, this uniquifier is removed from the list of uniquifiers (this does not cause
+        an error).
         :return: 0 if topology is valid, 1 otherwise
         """
         participant_names: set[str] = set()
@@ -74,11 +76,20 @@ class TopologyReader:
 
         # Check if participants actually appear in exchanges
         participants_in_exchanges: set[str] = set()
+        # Check if exchanges are unique
+        known_exchanges: set[tuple[str, str, str]] = set()
+        # Check if locations of a participant have only one type
+        participant_location_type_map: dict[tuple[str, str], str] = {}
 
         # Check if exchanges only contain known "to" and "from" participants
         for exchange in self.topology["exchanges"]:
             to_participant: str = exchange["to"]
             from_participant: str = exchange["from"]
+            data: str = exchange["data"]
+            from_location_name: str = exchange["from-location-name"]
+            to_location_name: str = exchange["to-location-name"]
+            from_location_type: str = exchange.get("from-location-type")
+            to_location_type: str = exchange.get("to-location-type")
 
             participants_in_exchanges.add(to_participant)
             participants_in_exchanges.add(from_participant)
@@ -91,11 +102,36 @@ class TopologyReader:
                 logger.critical(f"Unknown participant {from_participant} in topology file "
                                 f"{self.topology_file_path}.")
                 return 1
-            data: str = exchange["data"]
 
             if from_participant == to_participant:
-                logger.error(f"Participant {from_participant} exchanges {data} with itself.")
+                logger.critical(f"Participant {from_participant} exchanges {data} with itself.")
                 return 1
+
+            # Check if the exchanges are unique when ignoring certain attributes
+            exchange_info: tuple[str, str, str] = (to_participant.lower(), from_participant.lower(), data.lower())
+            if exchange_info in known_exchanges:
+                logger.critical(f"Duplicate exchange from {from_participant} to {to_participant} for data {data}.")
+                return 1
+            known_exchanges.add(exchange_info)
+
+            # Gather the location types of the locations of the participant only if they were defined in the topology
+            # and check if they are unique
+            if from_location_type is not None:
+                from_location: tuple[str, str] = (from_participant, from_location_name)
+                if from_location in participant_location_type_map and participant_location_type_map[
+                    from_location] != from_location_type:
+                    logger.critical(f"Participant {from_participant} has multiple location types for location "
+                                    f"{from_location_name}.")
+                    return 1
+                participant_location_type_map[from_location] = from_location_type
+            if to_location_type is not None:
+                to_location: tuple[str, str] = (to_participant, to_location_name)
+                if to_location in participant_location_type_map and participant_location_type_map[
+                    to_location] != to_location_type:
+                    logger.critical(f"Participant {to_participant} has multiple location types for location "
+                                    f"{to_location_name}.")
+                    return 1
+                participant_location_type_map[to_location] = to_location_type
 
             # Remove uniquifiers from the list if they are present in a data name
             for uniquifier in helper.DATA_UNIQUIFIERS.copy():
@@ -108,6 +144,19 @@ class TopologyReader:
                 logger.warning(f"Removing participant {participant['name']} as it is defined but never used.")
                 self.topology["participants"].remove(participant)
 
+        # Now we are guaranteed that all participants locations have the same type.
+        # We can now write this back into the topology to make further processing easier.
+        for exchange in self.topology["exchanges"]:
+            from_participant: str = exchange["from"]
+            to_participant: str = exchange["to"]
+            from_location_name: str = exchange["from-location-name"]
+            to_location_name: str = exchange["to-location-name"]
+            # There is a maximum of one location type for each participant defined in the topology,
+            # so we can just take it or use the default location type if it is not defined in the topology.
+            exchange["from-location-type"] = participant_location_type_map.get((from_participant, from_location_name),
+                                                                               helper.DEFAULT_LOCATION_TYPE)
+            exchange["to-location-type"] = participant_location_type_map.get((to_participant, to_location_name),
+                                                                             helper.DEFAULT_LOCATION_TYPE)
         logger.debug("Topology does not contain any errors.")
         return 0
 
